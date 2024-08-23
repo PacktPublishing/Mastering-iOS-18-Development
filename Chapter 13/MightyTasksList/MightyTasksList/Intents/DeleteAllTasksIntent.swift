afeeb78fb91ca1252d196b1881a8f38672a64714
Avi Tsadok <avi.tsadok@gmail.com>
2024-08-23 19:56:02 +0300

    Update DeleteAllTasksIntent.swift

--- Chapter 13/MightyTasksList/MightyTasksList/Intents/DeleteAllTasksIntent.swift
+++ Chapter 13/MightyTasksList/MightyTasksList/Intents/DeleteAllTasksIntent.swift
@@ -10,7 +10,7 @@ struct DeleteAllTasksIntent: AppIntent {
             return .result(dialog: .init("Forry, there are no tasks to delete"))
         }
         
-        try await requestConfirmation(actionName: .go, dialog: IntentDialog("Are you sure you want to delete all of your tasks?"))
+        try await requestConfirmation(actionName: .go, dialog: IntentDialog("Are you sure you want to delete all your tasks?"))
                                                          
         TaskManager.shared.deleteAllTasks()
         return .result(dialog: .init("All of your tasks have been deleted."))
